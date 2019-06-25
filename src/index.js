96cb0e43174d7e78131dacdd47d1bb11c1e8c363
Miguel Ramiro <miguel@mramiro.com>
2019-06-24 21:52:36 -0700

    Express boilerplate

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import 'dotenv/config';
+import express from 'express';
+import router from './router';
 
-console.log("Hello world!");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use('/drop_token', router);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
